fbc39ba429f4182a924da48e89384db3d8f597d4
Keith T. Star <uberfoo@me.com>
2022-12-17 05:16:30 -0700

    An attempt at a README

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,20 @@
+//! CowVec: A vector of CoW elements
+//! The intended purpose of this implementation is to provide a means to create
+//! cheap references to the elements in another `Vec`. As long as there is no
+//! mutation, access is fast, and the memory cost is low.
+//!
+//! If you want to mutate an element, we utilize [XuderCow] to create a new Boxed
+//! element which is mutable, and does not effect the other `Vec`.
+//!
+//! This implementation uses unsafe code to store the elements. We allocate
+//! memory by means of a `Vec`, who's len is always zero.
+//!
+//! I wrote this partly for fun, but mostly because I needed a collection that
+//! I could implement mutable iterators on. I was also hoping that I could get
+//! mutable and immutable references to elements at the same time. Only because
+//! the `XuderCow` can change its referent to a `Box<T>` without invalidating the
+//! references that were pointing at the `&T`. Maybe I can do something tricky
+//! with lifetimes.
 use core::{
     cmp::PartialEq,
     fmt::Debug,
@@ -7,22 +24,6 @@ use core::{
 
 /// A vector with CoW elements
 ///
-/// The intended purpose of this implementation is to provide a means to create
-/// cheap references to the elements in another `Vec`. As long as there is no
-/// mutation, access is fast, and the memory cost is low.
-///
-/// If you want to mutate an element, we utilize XuderCow to create a new Boxed
-/// element which is mutable, and does not effect the other `Vec`.
-///
-/// This implementation uses unsafe code to store the elements. We allocate
-/// memory by means of a `Vec`, who's len is always zero.
-///
-/// I wrote this partly for fun, but mostly because I needed a collection that
-/// I could implement mutable iterators on. I was also hoping that I could get
-/// mutable and immutable references to elements at the same time. Only because
-/// the [XuderCow] can change its referent to a `Box<T>` without invalidating the
-/// references that were pointing at the `&T`. Maybe I can do something tricky
-/// with lifetimes.
 #[derive(Debug)]
 pub struct CowVec<'a, T>
 where
